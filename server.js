696d0a71a0658abc4017038fe6f560d1fd13f0a4
Himanshu Negi <ecoworld007@gmail.com>
2018-04-23 03:40:17 +0530

    used handler bar partials and helpers

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
 
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
 app.get('/', (req, res) => {
     res.render('home.hbs',{
-        pageTitle: 'About Page',
+        pageTitle: 'Home Page',
         currentYear: new Date().getFullYear(),
         message: 'Weclome to my home page'
     });
